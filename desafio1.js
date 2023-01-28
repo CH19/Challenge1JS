3c207e60acbfa4d8b731dc5c75a6fb392afe02e5
christian <chrislacruz2005@gmail.com>
2023-01-28 14:37:56 -0400

    aqui el desafio esta completado segun mis criterios sin embargo añadire algo mas en caso de que lo considere

---
+++ desafio1.js
@@ -0,0 +1,10 @@
+/*Primer desafio de JS */
+"user strict"
+
+var Name = "Christian";
+var IdPerson = 31612790;
+var Ungry = true;
+
+console.warn("El nombre = " + Name);
+console.warn("la cedula = " + IdPerson);
+console.warn(Name + " ¿Tiene hambre?=" + Ungry);
